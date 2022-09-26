f05aeea8c2987677fea7a26e1edfcb500fc1b148
LTam4s <114070808+LTam4s@users.noreply.github.com>
2022-09-26 10:29:07 +0200

    3.0

--- test/Program.cs
+++ test/Program.cs
@@ -12,6 +12,7 @@ namespace test
         {
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Max(a, b));
         }
     }
 }
